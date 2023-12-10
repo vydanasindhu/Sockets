3c845a73fd5ec5f4bf4b6a4fdeeeca58febe377e
Replit user <>
2023-12-10 20:44:03 +0000

    Removing comments

--- client/src/Components/GameDiscussion.js
+++ client/src/Components/GameDiscussion.js
@@ -1,25 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
+import React, { useState, useEffect } from "react";
+import "../App.css";
 export var interaction_cnt = 0;
+export var interaction_msgs = [];
 
-function GameDiscussion({ question, currentRound, funccompletediscussion, funcToGameStart }) {
+function GameDiscussion({
+  question,
+  currentRound,
+  funccompletediscussion,
+  funcToGameStart,
+}) {
   const [ws, setWs] = useState(null);
   const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   useEffect(() => {
-    const newWs = new WebSocket('wss://sockets.vydanasindhu.repl.co/');
+    const newWs = new WebSocket("wss://sockets.vydanasindhu.repl.co/");
     newWs.onmessage = (event) => {
       if (event.data instanceof Blob) {
         const reader = new FileReader();
-        reader.onload = function() {
+        reader.onload = function () {
           if (reader.result) {
-            setMessages(prev => [...prev, { text: reader.result.toString(), type: 'received' }]);
+            setMessages((prev) => [
+              ...prev,
+              { text: reader.result.toString(), type: "received" },
+            ]);
+            interaction_msgs.push({ text: reader.result.toString(), type: "received" });
           }
         };
         reader.readAsText(event.data);
       } else {
-        setMessages(prev => [...prev, { text: event.data, type: 'received' }]);
+        setMessages((prev) => [
+          ...prev,
+          { text: event.data, type: "received" },
+        ]);
+        interaction_msgs.push({ text: event.data, type: "received" });
       }
     };
     setWs(newWs);
@@ -30,8 +44,9 @@ function GameDiscussion({ question, currentRound, funccompletediscussion, funcTo
     interaction_cnt += 1;
     if (ws && ws.readyState === ws.OPEN && input) {
       ws.send(input);
-      setMessages(prev => [...prev, { text: input, type: 'sent' }]);
-      setInput('');
+      setMessages((prev) => [...prev, { text: input, type: "sent" }]);
+      interaction_msgs.push({ text: input, type: "sent" });
+      setInput("");
     }
   };
 
@@ -39,9 +54,15 @@ function GameDiscussion({ question, currentRound, funccompletediscussion, funcTo
     <div>
       <div className="App1">
         <div className="QuestionInfo">
-          <p className="Question"><b>Question:</b> {question.question}</p>
-          <p className='Answer'><b>Answer:</b> {question.answer}</p>
-          <p className='Source'><b>Source:</b> <a href={question.source}>Link</a></p>
+          <p className="Question">
+            <b>Question:</b> {question.question}
+          </p>
+          <p className="Answer">
+            <b>Answer:</b> {question.answer}
+          </p>
+          <p className="Source">
+            <b>Source:</b> <a href={question.source}>Link</a>
+          </p>
         </div>
         <div className="Chat2">
           <div className="starter-questions">
@@ -50,29 +71,44 @@ function GameDiscussion({ question, currentRound, funccompletediscussion, funcTo
             <p>What was the key clue that helped you guess the answer?</p>
             <p>What are your thoughts about the clue given?</p>
             <p className="starter-qns">For Clue Giver:</p>
-            <p>What did you find most challenging about avoiding the taboo words in this round?</p>
+            <p>
+              What did you find most challenging about avoiding the taboo words
+              in this round?
+            </p>
             <p>How did you strategize your clues for this topic?</p>
           </div>
           <div className="Messages">
             {messages.map((message, index) => (
-              <div key={index} className={`Message ${message.type === 'sent' ? 'Sent' : 'Received'}`}>
+              <div
+                key={index}
+                className={`Message ${
+                  message.type === "sent" ? "Sent" : "Received"
+                }`}
+              >
                 <p>{message.text}</p>
               </div>
             ))}
           </div>
-          <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type a message..." />
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message..."
+          />
           <button onClick={sendMessage}>Send</button>
         </div>
       </div>
       <div className="next">
         {/* Conditional rendering based on the current round */}
         {currentRound < 5 ? (
-          <button className='nextQuestion' onClick={funcToGameStart}>Next Question</button>
+          <button className="nextQuestion" onClick={funcToGameStart}>
+            Next Question
+          </button>
         ) : (
-          <button className="survey" onClick={funccompletediscussion}>Survey</button>
+          <button className="survey" onClick={funccompletediscussion}>
+            Survey
+          </button>
         )}
       </div>
-
     </div>
   );
 }
--- client/src/Components/SurveyComp.js
+++ client/src/Components/SurveyComp.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
-import { updateDoc } from 'firebase/firestore';
-import { docReff } from './MainContent';
-import { interaction_cnt } from './GameDiscussion';
+import React, { useState } from "react";
+import { updateDoc } from "firebase/firestore";
+import { docReff } from "./MainContent";
+import { interaction_cnt } from "./GameDiscussion";
+import { interaction_msgs } from "./GameDiscussion";
 
 function SurveyComp({ funccompletesurvey }) {
-
   const [answers2, setAnswers2] = useState({
     question5: 5,
     question6: 5,
     question7: 5,
     question8: 5,
-
   });
 
-  const [textquestion3, setTextquestion3] = useState('');
-  const [textquestion4, setTextquestion4] = useState('');
+  const [textquestion3, setTextquestion3] = useState("");
+  const [textquestion4, setTextquestion4] = useState("");
 
   const handleSliderChange2 = (e) => {
     setAnswers2({ ...answers2, [e.target.name]: parseInt(e.target.value) });
@@ -35,6 +34,7 @@ function SurveyComp({ funccompletesurvey }) {
     try {
       const combinedUpdates = {
         interaction_count: interaction_cnt,
+        interaction_messages: interaction_msgs,
         textquestion3: textquestion3,
         textquestion4: textquestion4,
         ...answers2,
@@ -44,18 +44,18 @@ function SurveyComp({ funccompletesurvey }) {
       await updateDoc(docReff, combinedUpdates);
       console.log("Last Survey - Document written with ID: ", docReff.id);
       funccompletesurvey();
-
     } catch (e) {
-      console.error('Error adding document: ', e);
-    };
-  };
+      console.error("Error adding document: ", e);
+    }
+  }
 
   return (
     <div class="thank-you-card">
       <div className="form-container">
         <form onSubmit={handleeSubmit2}>
           <label className="form-label">
-            What do you think about the benefits that  AI can bring to healthcare ? Rate from 1 to 10 on How optimistic are you about it?
+            What do you think about the benefits that AI can bring to healthcare
+            ? Rate from 1 to 10 on How optimistic are you about it?
             <br />
             <input
               type="text"
@@ -67,17 +67,22 @@ function SurveyComp({ funccompletesurvey }) {
             <br />
             <div className="slider-card">
               <p>Least Optimistic</p>
-              <input type="range" name="question5" min="1"
-                max="10" value={answers2.question5} onChange={handleSliderChange2} />
+              <input
+                type="range"
+                name="question5"
+                min="1"
+                max="10"
+                value={answers2.question5}
+                onChange={handleSliderChange2}
+              />
               <p>Most Optimistic</p>
             </div>
-
             <div className="score-display">Score: {answers2.question5}</div>
           </label>
           <br />
           <label className="form-label">
-            What are your views about the risks and challenges
-            AI might pose in healthcare? Rate from 1 to 10 on How risky AI can be in healthcare
+            What are your views about the risks and challenges AI might pose in
+            healthcare? Rate from 1 to 10 on How risky AI can be in healthcare
             <br />
             <input
               type="text"
@@ -89,8 +94,14 @@ function SurveyComp({ funccompletesurvey }) {
             <br />
             <div className="slider-card">
               <p>Low Risk</p>
-              <input type="range" name="question6" min="1"
-                max="10" value={answers2.question6} onChange={handleSliderChange2} />
+              <input
+                type="range"
+                name="question6"
+                min="1"
+                max="10"
+                value={answers2.question6}
+                onChange={handleSliderChange2}
+              />
               <p>High Risk</p>
             </div>
             <div className="score-display">Score: {answers2.question6}</div>
@@ -98,41 +109,52 @@ function SurveyComp({ funccompletesurvey }) {
           <br />
           <br />
           <label className="form-label">
-            How strongly do you belive that AI will speed up innovation
-            in developing new treatments and drugs?
+            How strongly do you belive that AI will speed up innovation in
+            developing new treatments and drugs?
             <br />
             <div className="slider-card">
               <p>Strongly Disagree</p>
-              <input type="range" name="question7" min="1" max="10"
-                value={answers2.question7} onChange={handleSliderChange2} />
+              <input
+                type="range"
+                name="question7"
+                min="1"
+                max="10"
+                value={answers2.question7}
+                onChange={handleSliderChange2}
+              />
               <p>Strongly Agree</p>
             </div>
-
             <div className="score-display">Score: {answers2.question7}</div>
           </label>
           <br />
           <br />
           <label className="form-label">
-            How significantly do you think AI will transform the
-            healthcare industry in the next decade?
+            How significantly do you think AI will transform the healthcare
+            industry in the next decade?
             <br />
-
             <div className="slider-card">
               <p>Not Significant</p>
-              <input type="range" name="question8" min="1" max="10"
-                value={answers2.question8} onChange={handleSliderChange2} />
+              <input
+                type="range"
+                name="question8"
+                min="1"
+                max="10"
+                value={answers2.question8}
+                onChange={handleSliderChange2}
+              />
               <p>Highly Significant</p>
-            </div >
+            </div>
             <div className="score-display">Score: {answers2.question8}</div>
           </label>
           <br />
           <br />
-          <button type="submit" className='button'>Submit</button>
+          <button type="submit" className="button">
+            Submit
+          </button>
         </form>
       </div>
-    </div >
+    </div>
   );
 }
-
 
 export default SurveyComp;
